98270798aa4d6c725b181299d0f712c6d641eeae
Andres Gutierrez <andresfelipegutierrez04@gmail.com>
2026-08-12 10:55:09 -0500

    Conexion base de datos

---
+++ src/config/credentials.js
@@ -0,0 +1,9 @@
+export const DB_NAME = "tikets_reporte_node"
+export const DB_USER = "root"
+export const DB_PASS = ""
+export const OBJ_CONN = {
+    port : 3306,
+    host :  "localhost",
+    dialect: "mysql"
+}
+
