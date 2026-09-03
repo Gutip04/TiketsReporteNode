89853b90cc14fbd605080e6ab658b517349fab6b
Andres Gutierrez <andresfelipegutierrez04@gmail.com>
2026-09-02 21:31:58 -0500

    Implementacion de RoutesUser.js

---
+++ src/Routes/RoutesUser.js
@@ -0,0 +1,10 @@
+import { Router } from "express"
+import { list, create, destroy, searchName, update } from "../controllers/ControllerUser.js"
+const router = Router()
+
+router.get("/user",list)
+router.get("/user/:name",searchName)
+
+router.post("/user",create)
+router.delete("/user/:id",destroy)
+router.patch("/user/:id",update)
