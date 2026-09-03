8a2c85d90919f96c3a83870edc45b2ffeadf79c4
Andres Gutierrez <andresfelipegutierrez04@gmail.com>
2026-09-02 20:24:29 -0500

    se implemento servicesRol.js

---
+++ src/services/ServicesRol.js
@@ -0,0 +1,42 @@
+import { createRol, getRol, updateRol, deleteRol, getRolName } from "../repositorys/RepositoryRol";
+
+// register one Rol
+export function registerRol(data) {
+
+    if (!data.name) {
+        return new Promise.reject(new Error("Incomplete Fields..."))
+    }
+    return createRol(data)
+}
+
+
+//  list all Rol
+export function listRol() {
+    return getRol()
+}
+
+// update Rol
+export function modifyRol(data, id) {
+    if (!id) {
+        return new Promise.reject(new Error("The ID is required..."))
+    }
+
+    return updateRol(data)
+}
+
+
+// delete Rol
+export function deleteRol(id) {
+    if (!id) {
+        return new Promise.reject(new Error("The ID is required..."))
+    }
+}
+
+// search Rol by Name
+export function searchRolByName(name) {
+    if (!name) {
+        return new Promise.reject(new Error("The Name is required..."))
+    }
+
+    return getRolName(name)
+}
