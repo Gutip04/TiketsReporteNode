7475655da9049e412b0ed2db7a43ee14cbb843e9
Andres Gutierrez <andresfelipegutierrez04@gmail.com>
2026-09-02 20:52:51 -0500

    implementacion ControllerRol.js

---
+++ src/controllers/ControllerRol.js
@@ -0,0 +1,59 @@
+import { registerRol, deleteRol, listRol, modifyRol, searchRolByName } from "../services/ServicesRol.js";
+
+
+// create Rol
+export function Create(req, res) {
+    registerRol(req.body)
+        .then((response) => {
+            res.status(201).json(response)
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message })
+        })
+}
+
+// list All Rol
+export function list(req, res) {
+    listRol()
+        .then((response) => {
+            res.status(200).json(response)
+        })
+        .catch((error) => {
+            res.status(500).json({ error: error.message })
+        })
+}
+
+// search one Rol
+export function searchName(req, res) {
+    searchRolByName(req.body.name)
+        .then((response) => {
+            res.status(200).json(response)
+        })
+        .catch((error) => {
+            res.status(500).json({ error: error.message })
+        })
+}
+
+
+// Update Deparment
+export function update(req, res) {
+    modifyRol(req.body, req.params.id)
+        .then((response) => {
+            res.status(201).json({ message: "Rol created successfully " })
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message })
+        })
+}
+
+
+// delete Deparment
+export function destroy(req, res) {
+    deleteRol(req.params.id)
+        .then((response) => {
+            res.status(201).json({ message: "Rol destroy successfully " })
+        })
+        .catch((error) => {
+            res.status(400).json({ error: error.message })
+        })
+}
