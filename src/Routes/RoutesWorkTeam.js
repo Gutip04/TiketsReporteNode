2251d07cd565dba8987961295986a65c40f09518
Esteban <2008esteban12@gmail.com>
2026-09-08 20:34:40 -0500

    se agrego el metodo delete en el http de manera exitosa y se modifico RoutesWorkTeam.js ya que habia un error

--- src/Routes/RoutesWorkTeam.js
+++ src/Routes/RoutesWorkTeam.js
@@ -6,7 +6,7 @@ router.get("/WorkTeam", list)
 router.get("/WorkTeam/:name", searchName)
 
 router.post("/WorkTeam", create)
-router.delete("/user/:id", destroy)
-router.patch("/user/:id", update)
+router.delete("/WorkTeam/:id", destroy)
+router.patch("/WorkTeam/:id", update)
 
 export default router
