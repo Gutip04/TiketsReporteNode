6e017c0a13609ecd094c49e57663116bc137c9fd
Esteban <2008esteban12@gmail.com>
2026-09-08 19:49:06 -0500

    Se hizo la parte de workTeam en index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import departemntRoutes from "./src/Routes/RoutesDepartment.js"
 import specializationRoutes from "./src/Routes/RoutesSpecialization.js"
 import rolRoutes from "./src/Routes/RoutesRol.js"
 import userRoutes from "./src/Routes/RoutesUser.js"
+import { WorkTeam } from "./src/Routes/RoutesWorkTeam.js";
 
 
 const app = express()
@@ -14,6 +15,7 @@ app.use("/api",departemntRoutes)
 app.use("/api",specializationRoutes)
 app.use("/api",rolRoutes)
 app.use("/api",userRoutes)
+app.use("/api", WorkTeamRoutes)
 
 
 const PORT = 3000
