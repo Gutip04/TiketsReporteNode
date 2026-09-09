4e73322fbc94d37d9b966e47af32c96f659bd22c
Esteban <2008esteban12@gmail.com>
2026-09-09 09:38:12 -0500

    Se agrego el repository de Priority

---
+++ src/repositorys/RepositoryPriority.js
@@ -0,0 +1,24 @@
+import { priority } from "../models/ModelPriority.js";
+//create priority
+export function createPriority(data){
+    return priority.create(data)
+}
+
+//Get all priority
+export function getPriority(data){
+    return priority.findAll(data)
+}
+
+//Update priority
+export function updatePriority(data, id){
+    return priority.update(data,{
+        where:{id_priority: id}
+    })
+}
+
+//delete priority
+export function deletePriority(id){ 
+    return priority.destroy({
+        where: {id_priority: id}
+    })
+}
