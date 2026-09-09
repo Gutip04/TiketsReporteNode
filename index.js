4b8bd1b3ef5a0eb87512c7bda169bc2146ff1d15
Didier Cano <didiercanoarroyave7@gmail.com>
2026-09-09 17:25:55 -0500

    RoutesStates_ticket.js implementado

--- index.js
+++ index.js
@@ -5,26 +5,23 @@ import departmentRoutes from "./src/Routes/RoutesDepartment.js";
 import specializationRoutes from "./src/Routes/RoutesSpecialization.js";
 import rolRoutes from "./src/Routes/RoutesRol.js";
 import userRoutes from "./src/Routes/RoutesUser.js";
-import StatesTicketRoute from "./src/Routes/RoutesStates_ticket.js";
-import TeamUserroute from "./src/Routes/RoutesTeam_user.js";
+
 import workTeamRoutes from "./src/Routes/RoutesWorkTeam.js";
 
 const app = express();
 
 app.use(express.json());
 
-// RUTAS
-// ==============================
+
 
 app.use("/api", departmentRoutes);
 app.use("/api", specializationRoutes);
-app.use("/api", StatesTicketRoute);
+
 app.use("/api", rolRoutes);
 app.use("/api", userRoutes);
 app.use("/api", workTeamRoutes);
 
-// Relaciones usuario - equipo
-app.use("/api/team_user", TeamUserroute);
+
 
 const PORT = 3000;
 const SERVER = "http://localhost:";
