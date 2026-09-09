25bc5d128dc6acbf2907126fc474b080c3124b59
Didier Cano <didiercanoarroyave7@gmail.com>
2026-09-08 23:14:42 -0500

    casi terminada la funcionalidad de Equipo_usuario

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
-import express from "express"
+import express from "express";
 import { conn } from "./src/config/database.js";
-import departemntRoutes from "./src/Routes/RoutesDepartment.js"
-import specializationRoutes from "./src/Routes/RoutesSpecialization.js"
-import rolRoutes from "./src/Routes/RoutesRol.js"
-import userRoutes from "./src/Routes/RoutesUser.js"
+
+import departemntRoutes from "./src/Routes/RoutesDepartment.js";
+import specializationRoutes from "./src/Routes/RoutesSpecialization.js";
+import rolRoutes from "./src/Routes/RoutesRol.js";
+import userRoutes from "./src/Routes/RoutesUser.js";
 import StatesTicketRoute from "./src/Routes/RoutesStates_ticket.js";
+import TeamUserroute from "./src/Routes/RoutesTeam_user.js";
+
+const app = express();
 
 
-const app = express()
-app.use(express.json())
+app.use(express.json());
 
 
-app.use("/api",departemntRoutes)
-app.use("/api",specializationRoutes)
-app.use("/api",StatesTicketRoute)
-app.use("/api",rolRoutes)
-app.use("/api",userRoutes)
+// RUTAS
+// ==============================
 
+// Relaciones usuario - equipo
+app.use("/api/team_user", TeamUserroute);
 
-const PORT = 3000
-const SERVER = "http://localhost:"
-const URL = SERVER + PORT
+// Otras rutas
+app.use("/api", departemntRoutes);
+app.use("/api", specializationRoutes);
+app.use("/api", StatesTicketRoute);
+app.use("/api", rolRoutes);
+app.use("/api", userRoutes);
+
+const PORT = 3000;
+const SERVER = "http://localhost:";
+const URL = SERVER + PORT;
 
 app.listen(PORT, () => {
-    console.log("Servidor funcionando de forma correcta. URL: " + URL)
-})
+    console.log(
+        "Servidor funcionando de forma correcta. URL: " + URL
+    );
+});
 
- 
+
 conn.authenticate()
-    .then(() =>{
-        return conn.sync()
-        
+    .then(() => {
+        return conn.sync();
     })
-    .catch((error) =>{
-        console.log("error a conexion de la base de datos: " + error);
-        
-    })
+    .catch((error) => {
+        console.log(
+            "Error en la conexión de la base de datos: ",
+            error
+        );
+    });
--- src/models/relations.js
+++ src/models/relations.js
@@ -3,6 +3,7 @@ import { User } from "./modelUser.js";
 import { Rol } from "./ModelRol.js";
 import { Specialization } from "./ModelSpecialization.js";
 import { StatesTicket } from "./modelStates_ticket.js";
+import {TeamUser } from "./ModelTeam_user.js";
 
 Rol.hasMany(User, {foreignKey: "id_rol"})
 User.belongsTo(Rol,{foreignKey: "id_rol"})
@@ -13,6 +14,10 @@ User.belongsTo(Department, {foreignKey: "id_departament"})
 Specialization.hasMany(User, {foreignKey: "id_specialization"})
 User.belongsTo(Specialization, {foreignKey: "id_specialization"})
 
+
+User.belongsToMany(Equipo, { through: TeamUser, foreignKey: "id_user" })
+Equipo.belongsToMany(User, { through: TeamUser, foreignKey: "id_Team" }) 
+
 export  {
-    Rol, Department, User, Specialization, StatesTicket
+    Rol, Department, User, Specialization, StatesTicket,TeamUser
 }
