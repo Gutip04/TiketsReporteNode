c1265e3623e2376cd1bad0797dcc70c6b39a45a9
jeronimopelaezrios6-code <jeronimopelaezrios6@gmail.com>
2026-09-11 17:08:05 -0500

    Implementacion de relaciones (Foreignkeys) de la tabla Tickets

--- src/models/relations.js
+++ src/models/relations.js
@@ -7,6 +7,8 @@ import { TeamUser } from "./ModelTeam_user.js";
 import { WorkTeam } from "./ModelWorkTeam.js";
 import { SupportDevice } from "./ModelSupportDevice.js";
 import { Support } from "./ModelSupport.js";
+import { Ticket } from "./ModelTicket.js";
+import { priority } from "./ModelPriority.js";
 
 Rol.hasMany(User, { foreignKey: "id_rol" });
 User.belongsTo(Rol, { foreignKey: "id_rol" });
@@ -36,7 +38,7 @@ SupportDevice.belongsTo(Department, {
 });
 
 
-//Relation DevePelaez Support
+//Relations Support (Branch : DevePelaez)
 //User - Support 
 User.hasMany(Support, {
     foreignKey: "id_user"
@@ -46,6 +48,74 @@ Support.belongsTo(User, {
 });
 
 //Ticket - Support
+Ticket.hasMany(Support, {
+    foreignKey: "id_ticket"
+});
+Ticket.belongsTo(Support,{
+    foreignKey: "id_ticket"
+});
+
+
+//Relations Ticket (Branch : DevePelaez)
+//Creator_User - Ticket
+User.hasMany(Ticket, {
+    foreignKey: "id_creator_user",
+    as: "TicketCreated"
+});
+Ticket.belongsTo(User, {
+    foreignKey: "id_creator_user",
+    as: "CreatorUser"
+});
+
+//Assigned_User - Ticket
+User.hasMany(Ticket, {
+    foreignKey: "id_assigned_user",
+    as: "TicketAssigned"
+});
+Ticket.belongsTo(User, {
+    foreignKey: "id_assigned_user",
+    as: "AssignedUser"
+});
+
+//Department - Ticket
+Department.hasMany(Ticket, {
+    foreignKey: "id_department"
+});
+Ticket.belongsTo(Department, {
+    foreignKey: "id_department"
+});
+
+//WorkTeam - Ticket
+WorkTeam.hasMany(Ticket, {
+    foreignKey: "id_WorkTeam"
+});
+Ticket.belongsTo(WorkTeam, {
+    foreignKey: "id_WorkTeam"
+});
+
+//SupportDevice - Ticket
+SupportDevice.hasMany(Ticket, {
+    foreignKey: "id_device"
+});
+Ticket.belongsTo(SupportDevice, {
+    foreignKey: "id_device"
+})
+
+//Status - Ticket
+StatesTicket.hasMany(Ticket, {
+    foreignKey: "id_statusTicket"
+});
+Ticket.belongsTo(StatesTicket, {
+    foreignKey: "id_statusTicket"
+});
+
+//Priority - Ticket
+priority.hasMany(Ticket, {
+    foreignKey: "id_priority"
+});
+Ticket.belongsTo(priority, {
+    foreignKey: "id_priority"
+})
 
 export {
     Rol,
